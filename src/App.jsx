66bde5d4e4217189c67c49859d47d70bc752c345
leduyhau <le.duy.hau01@sky-tech.co.jp>
2024-07-23 23:20:03 +0900

    video28

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const MyComponent = () => {
+  return (
+    <div>le duy hau</div>
+  );
+}
 
 const App = () => {
   const [count, setCount] = useState(0)
@@ -18,6 +23,7 @@ const App = () => {
         </a>
       </div>
       <h1>Hello world</h1>
+      <MyComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
