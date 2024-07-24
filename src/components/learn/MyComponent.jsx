fbbf637bf440ff0cb58cbd72b1f61b89008fecee
leduyhau <le.duy.hau01@sky-tech.co.jp>
2024-07-24 10:16:40 +0900

    video31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,14 @@
 import "./style.css"
 const MyComponent = () => {
+    // const hoidanit =null;
+    const hoidanit = [1, 2, 3]
+    // const hoidanit = {
+    //     name: "hoidanit",
+    //     age: 25
+    // }
     return (
       <>
-        <div>le duy hau in japan</div>
+        <div>le duy hau {JSON.stringify(hoidanit)} in japan</div>
         <div className="child"
         style={{borderRadius: "10px"}}
         >abcd</div>
