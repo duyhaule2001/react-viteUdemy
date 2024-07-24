b57d51de5fe07fbcb009034260261dca2e71b49e
leduyhau <le.duy.hau01@sky-tech.co.jp>
2024-07-24 22:18:08 +0900

    video 34

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,17 @@ import TodoNew from "./components/todo/TodoNew";
 import reactLogo from "./assets/react.svg";
 
 const App = () => {
+  const hoidanit = "Hau";
+  const age = 23;
+  const data = {
+    address: "vinhphuc",
+    country: "vietnam",
+  };
   return (
     <div className="todo-container">
       <h1 className="todo-title">ToDo List</h1>
       <TodoNew />
-      <TodoData />
+      <TodoData name={hoidanit} age={age} data={data} />
       <div className="todo-image">
         <img className="logo" src={reactLogo} />
       </div>
