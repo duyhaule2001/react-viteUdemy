5ff96c104ea3925f188ea7380bb1d7f7a6b22210
leduyhau <le.duy.hau01@sky-tech.co.jp>
2024-07-24 09:58:12 +0900

    video30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,12 @@
+import "./style.css"
 const MyComponent = () => {
     return (
-      <div>le duy hau</div>
+      <>
+        <div>le duy hau in japan</div>
+        <div className="child"
+        style={{borderRadius: "10px"}}
+        >abcd</div>
+      </>
     );
   }
 
