e005a1d92c458117310639737a16a60038267b43
leduyhau <le.duy.hau01@sky-tech.co.jp>
2024-07-25 23:26:56 +0900

    video 47

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import "./components/todo/todo.css";
 import TodoNew from "./components/todo/TodoNew";
 import reactLogo from "./assets/react.svg";
 import { useState } from "react";
+import Header from "./components/layout/header";
+import Footer from "./components/layout/footer";
 
 const App = () => {
   const [todoList, setTodoList] = useState([]);
@@ -25,17 +27,21 @@ const App = () => {
   };
 
   return (
-    <div className="todo-container">
-      <h1 className="todo-title">ToDo List</h1>
-      <TodoNew addNewToDo={addNewToDo} />
-      {todoList.length === 0 ? (
-        <div className="todo-image">
-          <img className="logo" src={reactLogo} />
-        </div>
-      ) : (
-        <TodoData todoList={todoList} deleteTodo={deleteTodo} />
-      )}
-    </div>
+    <>
+      <Header />
+      <div className="todo-container">
+        <h1 className="todo-title">ToDo List</h1>
+        <TodoNew addNewToDo={addNewToDo} />
+        {todoList.length === 0 ? (
+          <div className="todo-image">
+            <img className="logo" src={reactLogo} />
+          </div>
+        ) : (
+          <TodoData todoList={todoList} deleteTodo={deleteTodo} />
+        )}
+      </div>
+      <Footer />
+    </>
   );
 };
 
