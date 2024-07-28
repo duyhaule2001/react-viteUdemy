4087beef2e71b3f3ab60cb2bcff194111507b786
leduyhau <le.duy.hau01@sky-tech.co.jp>
2024-07-28 23:28:15 +0900

    #73. useEffect Hook

--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -1,80 +1,56 @@
-import { Space, Table, Tag } from 'antd';
+import { Table } from "antd";
+import { fetchAllUserAPI } from "../../services/api.service";
+import { useEffect, useState } from "react";
 
 const UserTable = () => {
-    const columns = [
-        {
-          title: 'Name',
-          dataIndex: 'name',
-          key: 'name',
-          render: (text) => <a>{text}</a>,
-        },
-        {
-          title: 'Age',
-          dataIndex: 'age',
-          key: 'age',
-        },
-        {
-          title: 'Address',
-          dataIndex: 'address',
-          key: 'address',
-        },
-        {
-          title: 'Tags',
-          key: 'tags',
-          dataIndex: 'tags',
-          render: (_, { tags }) => (
-            <>
-              {tags.map((tag) => {
-                let color = tag.length > 5 ? 'geekblue' : 'green';
-                if (tag === 'loser') {
-                  color = 'volcano';
-                }
-                return (
-                  <Tag color={color} key={tag}>
-                    {tag.toUpperCase()}
-                  </Tag>
-                );
-              })}
-            </>
-          ),
-        },
-        {
-          title: 'Action',
-          key: 'action',
-          render: (_, record) => (
-            <Space size="middle">
-              <a>Invite {record.name}</a>
-              <a>Delete</a>
-            </Space>
-          ),
-        },
-      ];
-      const data = [
-        {
-          key: '1',
-          name: 'John Brown',
-          age: 32,
-          address: 'New York No. 1 Lake Park',
-          tags: ['nice', 'developer'],
-        },
-        {
-          key: '2',
-          name: 'Jim Green',
-          age: 42,
-          address: 'London No. 1 Lake Park',
-          tags: ['loser'],
-        },
-        {
-          key: '3',
-          name: 'Joe Black',
-          age: 32,
-          address: 'Sydney No. 1 Lake Park',
-          tags: ['cool', 'teacher'],
-        },
-      ];
-  return (
-    <Table columns={columns} dataSource={data} />
-  )
-}
+  const [dataUser, setDataUser] = useState([]);
 
-export default UserTable
+  useEffect(() => {
+    loadUSer();
+  }, []);
+  const columns = [
+    {
+      title: "Id",
+      dataIndex: "_id",
+    },
+    {
+      title: "Full Name",
+      dataIndex: "fullName",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+    },
+  ];
+
+  //   {
+  //     key: "1",
+  //     name: "John Brown",
+  //     age: 32,
+  //     address: "New York No. 1 Lake Park",
+  //     tags: ["nice", "developer"],
+  //   },
+  //   {
+  //     key: "2",
+  //     name: "Jim Green",
+  //     age: 42,
+  //     address: "London No. 1 Lake Park",
+  //     tags: ["loser"],
+  //   },
+  //   {
+  //     key: "3",
+  //     name: "Joe Black",
+  //     age: 32,
+  //     address: "Sydney No. 1 Lake Park",
+  //     tags: ["cool", "teacher"],
+  //   },
+  // ];
+  const loadUSer = async () => {
+    const res = await fetchAllUserAPI();
+    setDataUser(res.data);
+  };
+
+  return <Table columns={columns} dataSource={dataUser} rowKey={"_id"} />;
+};
+
+export default UserTable;
